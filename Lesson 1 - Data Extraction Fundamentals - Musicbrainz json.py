4916b884df0a8d074a4f02fc39756238133f4bc0
FlyingDad <mkroth65@gmail.com>
2014-04-15 20:18:50 -0500

    UD 032 Files

--- Lesson 1 - Data Extraction Fundamentals - Musicbrainz json.py
+++ Lesson 1 - Data Extraction Fundamentals - Musicbrainz json.py
@@ -18,7 +18,7 @@ query_type = {  "simple": {},
 def query_site(url, params, uid="", fmt="json"):
     params["fmt"] = fmt
     r = requests.get(url + uid, params=params)
-    print "requesting", r.url
+    #print "requesting", r.url
 
     if r.status_code == requests.codes.ok:
         return r.json()
@@ -40,13 +40,59 @@ def pretty_print(data, indent=4):
 
 
 def main():
-    results = query_by_name(ARTIST_URL, query_type["simple"], "queen")
-    print "Query reslts: "
+    #results = query_by_name(ARTIST_URL, query_type["simple"], "queen")
+    #print "Query reslts: "
     #pretty_print(results)
+    #print '======================================='
 
-    artist_id = results["artist"][1]["id"]
-    print "\nARTIST:"
-    pretty_print(results["artist"][1])
+    #how many bands named FIRST AID KIT
+    results = query_by_name(ARTIST_URL, query_type["simple"], "FIRST AID KIT")
+    #print len(results)
+    #pretty_print(results)
+    fakcount = 0
+    for artist in results["artist"]:
+        if artist["name"] == "First Aid Kit":
+            fakcount += 1
+    print "First Aid Kits: ", fakcount
+
+
+    print "=================================="
+    #begin_area for queen
+    results = query_by_name(ARTIST_URL, query_type["simple"], "queen")
+    #print type(results)
+    print "Queen begin location: ",
+    pretty_print(results["artist"][0]["begin-area"]["name"])
+
+    #spanish alias for beatles
+    results = query_by_name(ARTIST_URL, query_type["simple"], "beatles")
+    pretty_print(results)
+
+    q3 = query_by_name(ARTIST_URL, query_type["simple"], "Beatles")
+    aliases = q3["artist"][0]["aliases"]
+    alias = ''
+    for a in aliases:
+        if a["locale"] == "es":
+            alias = a["name"]
+    print "Q3: Spanish alias for Beatles"
+    print alias
+    #pretty_print(q3)
+
+    q4 = query_by_name(ARTIST_URL, query_type["simple"], "Nirvana")
+    disam = q4["artist"][0]["disambiguation"]
+    print "Q4: Disambiguation Nirvana"
+    print disam
+
+    q5 = query_by_name(ARTIST_URL, query_type["simple"], "One Direction")
+    begin_date = q5["artist"][0]["life-span"]["begin"]
+    print "Q5: One Direction begin"
+    print begin_date
+    #nirvana disambiguation
+    #when was one direction formed?
+
+
+    #artist_id = results["artist"][1]["id"]
+    #print "\nARTIST:"
+    #pretty_print(results["artist"][1])
     #
     #artist_data = query_site(ARTIST_URL, query_type["releases"], artist_id)
     #releases = artist_data["releases"]
